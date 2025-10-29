ad8b2ab7a9bb6d82e45530bcb0028a6d453f24c5
howardchao <howardchao@retronix.com.tw>
2025-10-29 10:35:32 +0800

    serial: fix hrtimer reinit issue in sh-sci driver
    Signed-off-by: Felix Hsu <felixhsu@retronix.com.tw>

--- drivers/tty/serial/sh-sci.c
+++ drivers/tty/serial/sh-sci.c
@@ -148,6 +148,7 @@ struct sci_port {
 	struct work_struct		work_tx;
 	struct hrtimer			rx_timer;
 	unsigned int			rx_timeout;	/* microseconds */
+	bool				rx_timer_inited;
 #endif
 	unsigned int			rx_frame;
 	int				rx_trigger;
@@ -1651,8 +1652,11 @@ static void sci_request_dma(struct uart_port *port)
 			dma += s->buf_len_rx;
 		}
 
-		hrtimer_init(&s->rx_timer, CLOCK_MONOTONIC, HRTIMER_MODE_REL);
-		s->rx_timer.function = sci_dma_rx_timer_fn;
+		if (!s->rx_timer_inited) {
+			hrtimer_init(&s->rx_timer, CLOCK_MONOTONIC, HRTIMER_MODE_REL);
+			s->rx_timer.function = sci_dma_rx_timer_fn;
+			s->rx_timer_inited = true;
+		}
 
 		s->chan_rx_saved = s->chan_rx = chan;
 
